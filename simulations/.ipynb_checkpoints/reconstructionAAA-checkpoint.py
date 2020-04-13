e1608c791c99958fbd1ec53594942c3018186103
emilyachang <emilyachang22@gmail.com>
2020-04-13 19:04:11 -0400

    simulations achieved :)

---
+++ simulations/.ipynb_checkpoints/reconstructionAAA-checkpoint.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+
+def reconstruct(img):
+    
+    
+    
+    return 
+
+def main():
+
+    img = cv2.imread('image.png', cv2.IMREAD_COLOR);
+    
+main();
